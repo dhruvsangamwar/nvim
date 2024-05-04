546dc42bbaec4f6c5fa2436251c57d9e1b9ff159
Dhruv Sangamwar <dsangamwar@gmail.com>
2024-05-04 14:26:01 -0700

    Initial commit

---
+++ lua/dsang/init.lua
@@ -0,0 +1,4 @@
+require("dsang.remap")
+require("dsang.lazy")
+require("dsang.set")
+require("dsang.remap")
---
+++ lua/dsang/lazy.lua
@@ -0,0 +1,49 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+return require("lazy").setup({
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	-- init.lua:
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                              , branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		config = true
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, 
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+})
+
